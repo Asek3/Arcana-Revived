3ab0565cd3f601f15a177bcc8fe73752c427acfd
Nano <bogban.zp@gmail.com>
2022-07-22 22:44:07 +0300

    Fixed Smokey Glass

--- src/main/java/net/arcanamod/blocks/bases/SolidVisibleBlock.java
+++ src/main/java/net/arcanamod/blocks/bases/SolidVisibleBlock.java
@@ -1,10 +1,19 @@
 package net.arcanamod.blocks.bases;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.block.BreakableBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
 
 public class SolidVisibleBlock extends BreakableBlock{
 	
 	public SolidVisibleBlock(Properties properties){
 		super(properties);
 	}
+	
+	@Override
+	public int getOpacity(BlockState state, IBlockReader world, BlockPos pos) {
+		return world.getMaxLightLevel();
+	}
+	
 }
